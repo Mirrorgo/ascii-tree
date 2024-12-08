a5eb99b84bc1b9f28859cb35ae8ffc665f62e813
mirrorgo <cloudmirrorgo@163.com>
2024-12-08 17:43:36 +0800

    fix: bug for tailwind css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+import animate from "tailwindcss-animate";
+
 export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
@@ -53,5 +56,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animate],
 };
